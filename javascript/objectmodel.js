ee1606912be516fa65956d379fe140d0d906d7eb
Andrew Farrell <afarrell@mit.edu>
2012-04-12 17:25:14 -0400

    added uniqeIDs to Activities.

--- javascript/objectmodel.js
+++ javascript/objectmodel.js
@@ -1,9 +1,13 @@
 (function() {
-  var Activity;
+  var Activity, activities;
+
+  activities = [];
 
   Activity = (function() {
 
-    function Activity(name, location, start, end, duration, range, user_createdP) {
+    Activity.id = 0;
+
+    function Activity(name, location, start, end, duration, range, user_createdP, scheduledP) {
       this.name = name;
       this.location = location;
       this.start = start;
@@ -11,14 +15,17 @@
       this.duration = duration;
       this.range = range;
       this.user_createdP = user_createdP;
-      $.jStorage.set(this.name, this);
+      this.scheduledP = scheduledP;
+      this.id = JSON.stringify(Activity.id);
+      Activity.id += 1;
+      $.jStorage.set(this.id, this);
     }
 
     return Activity;
 
   })();
 
-  [new Activity("lunch with Robert", "42.3656, -71.0215", "13:00", "14:00", 60, "12:00-14:30", true), new Activity("Walking the freedom trail", "42.2656, -71.0235", "14:15", "15:30", 75, "13:00-17:30", true), new Activity("Having a Pint", "42.3621, -71.0570", "15:45", "16:30", 45, "13:00-22:30", true), new Activity("Sailing", "42.3607, -71.0733", "09:45", "11:00", 75, "08:00-15:30", true), new Activity("Gawking at students in course III lab", "42.3603, -71.0958", "11:15", "12:00", 45, "11:00-16:30", true)];
+  [new Activity("lunch with Robert", "42.3656, -71.0215", "13:00", "14:00", 60, "12:00-14:30", true, false), new Activity("Walking the freedom trail", "42.2656, -71.0235", "14:15", "15:30", 75, "13:00-17:30", true, false), new Activity("Having a Pint", "42.3621, -71.0570", "15:45", "16:30", 45, "13:00-22:30", true, false), new Activity("Sailing", "42.3607, -71.0733", "09:45", "11:00", 75, "08:00-15:30", true, false), new Activity("Gawking at students in course III lab", "42.3603, -71.0958", "11:15", "12:00", 45, "11:00-16:30", true, false)];
 
   window.O = {
     'Activity': Activity
